f8b6893e2c02ad941dbd50bb50fc4a2efba0d64b
miguel.santos.silva.07@gmail.com <miguel.santos.silva.07@gmail.com>
2025-07-04 18:10:42 -0300

    Salvando

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
-# Nome do Meu Projeto
+# sv
 
-Projeto Final 2025 do Curso Técnico Integrado de Desenvolvimento de Sistemas - Colégio Pedro II - Campus Duque de Caxias
+Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
 
-**Integrantes:**
- - Fulana
- - Beltrano
- - Ciclano
+## Creating a project
 
- ## Tecnologias
+If you're seeing this, you've probably already done this step. Congrats!
 
-Este projeto é desenvolvido utilizando  para desenvolvimento da API de backend, SvelteKit como framework frontend e Tailwind como framework CSS.
+```bash
+# create a new project in the current directory
+npx sv create
 
-Em termos de arquitetura de software, este projeto é composto por duas aplicações:
-- API/Backend desenvolvida em Node.js com Express
-- Aplicação Frontend desenvolvida com Svelte e estilizada com Tailwind
+# create a new project in my-app
+npx sv create my-app
+```
 
-A Aplicação frontend realiza requisições à API utilizando os verbos HTTP, que por sua vez retorna as informações a serem tratadas pela interface. Todo envio e rebimento de informações entre as duas aplicações é realizada utilizando o formato JSON.
+## Developing
 
-Para detalhes técnicos de como executar o projeto consulte o [README da API](src/api/README.md) e [README da Aplicação Frontend](src/frontend-app/README.md). 
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
 
-## Descrição do Projeto
+```bash
+npm run dev
 
-Nunc aliquet lobortis tristique. In pretium nisi vitae viverra venenatis. Ut iaculis sem quis arcu ornare interdum. In id condimentum arcu, sed ornare sem. Aliquam consequat at dolor eget sagittis. Praesent nec ante eget felis porttitor cursus eget suscipit urna. Sed consequat consectetur tristique.
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
 
-Sed sit amet ante sem. Nunc id sapien metus. Etiam lobortis in purus ut porta. Vestibulum at libero euismod felis feugiat tempus eu sagittis lorem. Vestibulum ultrices, mi euismod eleifend viverra, odio enim volutpat tortor, sed condimentum odio est id purus. Sed at leo vel ligula luctus bibendum in sed justo. Duis sodales consectetur vestibulum. Morbi libero quam, varius ac sodales eget, bibendum eu quam. In accumsan ac turpis vitae porttitor. Nulla eget velit in nisl vulputate sagittis. Phasellus ligula mi, viverra id vehicula nec, varius eget diam. Vestibulum id ultricies nulla, vel sagittis lorem. Sed aliquam dictum lacus quis maximus.
+## Building
 
-## Documentação
+To create a production version of your app:
 
-- [Manual do Usuário](manual.md)
-- [Requisitos](requisitos.md)
-- [Casos de Uso](casos-de-uso.md)
-- [Apresentação](apresentacao.pdf)
+```bash
+npm run build
+```
 
-**Modelagem do Banco de Dados**
+You can preview the production build with `npm run preview`.
 
-![Diagrama de Banco de Dados](diagramas/diagrama-exemplo.png)
+> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,114 @@
+<table id="selection-table">
+    <thead>
+        <tr>
+            <th>
+                <span class="flex items-center">
+                    Nome do Livro
+                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th data-type="date" data-format="YYYY/DD/MM">
+                <span class="flex items-center">
+                    Data de Lançamento
+                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center">
+                    Número de Páginas
+                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center">
+                    Nome do Autor
+                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">A Culpa é das Estrelas</td>
+            <td>10/01/2012</td>
+            <td>255</td>
+            <td>John Green</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap d">O Alienista</td>
+            <td>15/03/1882</td>
+            <td>169</td>
+            <td>Machado de Assis</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">A Cartomante</td>
+            <td>1884</td>
+            <td>8</td>
+            <td>Machado de Assis</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">Conectadas</td>
+            <td>31/07/2019</td>
+            <td>77</td>
+            <td>Clara Alves</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">Romance Real</td>
+            <td>31/05/2022</td>
+            <td>68</td>
+            <td>Clara Alves</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">Verity</td>
+            <td>07/12/2018</td>
+            <td>34</td>
+            <td>Collen Hoover</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">Água Viva</td>
+            <td>23/08/1973</td>
+            <td>10</td>
+            <td>Clarice Lispector</td>
+        </tr>
+        <tr class="hover:bg-gray-50 dark:hover:bg-orange-100 cursor-pointer">
+            <td class="font-medium text-gray-900 whitespace-nowrap ">O Lustre</td>
+            <td>1946</td>
+            <td>31</td>
+            <td>Clarice Lispector</td>
+        </tr> 
+    </tbody>
+</table>
+<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5IrWjikFMlzn4QkCIFgA4AyxEeLdOabDW1A&s" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">V3AL</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-orange-300 sm:mb-0 dark:text-orange-200">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Sobre</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Política de Privacidade</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licença</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contate-nos</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-orange-300 sm:mx-auto dark:border-orange-200 lg:my-8" />
+        <span class="block text-sm text-orange-300 sm:text-center dark:text-orange-200">© 2025 <a href="https://flowbite.com/" class="hover:underline">V3AL</a>. All Rights Reserved.</span>
+    </div>
+</footer>
