0cc2e44445bb0f67de5f00565908858ad24122d7
miguel.santos.silva.07@gmail.com <miguel.santos.silva.07@gmail.com>
2025-09-04 15:21:24 -0300

    ok

--- src/frontend-app/src/routes/+page.svelte
+++ src/frontend-app/src/routes/+page.svelte
@@ -7,10 +7,15 @@
 <Menu />
 
 <div class="text-center p-8 pt-32">
-	<Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6">PAGINA INICIAL</Heading>
+    <img src= "/images/1000274060.png" class="me-3 h-6 sm:h-15" alt="" />
+<img src= "/images/1000274061.png" class="me-3 h-6 sm:h-15" alt="" />
+<img src= "/images/1000274062.png" class="me-3 h-6 sm:h-15" alt="" />
+<img src= "/images/1000274063.png" class="me-3 h-6 sm:h-15" alt="" />
+	<Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6">NOSSO CATÁLOGO</Heading>
 	<P class="text-lg leading-relaxed text-gray-700 dark:text-gray-300 mb-4 text-justify">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Mauris consequat, sapien et dictum cursus, urna erat laoreet enim, nec gravida urna erat at sapien. Etiam nec dolor vitae odio hendrerit venenatis. Proin nec turpis nec sem tempus dictum. Vivamus tincidunt, nisi nec facilisis cursus, enim erat dictum neque, at dignissim enim urna at velit. Integer non turpis id mi tristique cursus. Nullam in sem vitae massa dictum dictum.
+		Conheça nosso vasto catálogo de vinis!
 	</P>
+
 	<Skeleton class="mt-16 mb-8" />
 	<ImagePlaceholder class="my-8" />
 	<TextPlaceholder class="my-8" />
--- src/frontend-app/src/routes/users/edit/[id]/+page.svelte
+++ src/frontend-app/src/routes/users/edit/[id]/+page.svelte
@@ -10,3 +10,29 @@
 
 <!-- Utiliza o componente UserForm passando id -->
 <UserForm id={Number(id)} />
+
+
+
+<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+  <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img src="/images/1000285043 (1).png" class="h-8" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">vival</span>
+          </a>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                  <a href="/about" class="hover:underline me-4 md:me-6">Sobre</a>
+              </li>
+              <li>
+                  <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+              </li>
+              <li>
+                  <a href="#" class="hover:underline">Contact</a>
+              </li>
+          </ul>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="" class="hover:underline">Vival</a>. All Rights Reserved.</span>
+  </div>
+</footer>
