32019943f5604ef5f53fde3d724c1deab9697c29
miguel.santos.silva.07@gmail.com <miguel.santos.silva.07@gmail.com>
2025-09-25 17:42:15 -0300

    logout

--- src/frontend-app/src/routes/users/+page.svelte
+++ src/frontend-app/src/routes/users/+page.svelte
@@ -58,13 +58,6 @@
   <div class="flex items-center justify-between max-w-3xl mx-auto mb-6">
     <Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Usuários</Heading>
     
-    <button 
-      class="ml-2 px-4 py-2 bg-primary-600 items-center hover:bg-primary-700 text-gray rounded-lg text-sm font-semibold flex items-center gap-2 shadow transition"
-      on:click={handleLogout}
-    >
-    <ArrowRightToBracketOutline class="w-4 h-4" />
-      Logout
-    </button>
 
     <button class="flex items-center gap-2 px-4 py-2 bg-primary-600 hover:bg-primary-700 text-gray rounded-lg font-semibold shadow transition" on:click={() => goto('/users/new')}>
       <UserAddOutline class="w-5 h-5" />
