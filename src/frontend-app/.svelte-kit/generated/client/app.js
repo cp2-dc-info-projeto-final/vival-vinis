f8e93e3cc43e029c5cd1dbeee4494ad66bc364af
miguel.santos.silva.07@gmail.com <miguel.santos.silva.07@gmail.com>
2025-12-05 16:50:06 -0300

    pesquisa

---
+++ src/frontend-app/.svelte-kit/generated/client/app.js
@@ -0,0 +1,54 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9'),
+	() => import('./nodes/10'),
+	() => import('./nodes/11'),
+	() => import('./nodes/12'),
+	() => import('./nodes/13'),
+	() => import('./nodes/14'),
+	() => import('./nodes/15')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/about": [3],
+		"/cadastroproduto": [5],
+		"/cadastroproduto/edit/[id]": [6],
+		"/cadastroproduto/new": [7],
+		"/cadastro": [4],
+		"/carrinho": [8],
+		"/catálogo": [9],
+		"/concluir_compra": [10],
+		"/login": [11],
+		"/pedidos": [12],
+		"/users": [13],
+		"/users/edit/[id]": [14],
+		"/users/new": [15]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+	
+	reroute: (() => {}),
+	transport: {}
+};
+
+export const decoders = Object.fromEntries(Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]));
+
+export const hash = false;
+
+export const decode = (type, value) => decoders[type](value);
+
+export { default as root } from '../root.js';
---
+++ src/frontend-app/.svelte-kit/generated/client/nodes/7.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/cadastroproduto/new/+page.svelte";
